c35cc71118f3335539b948e11355979856853b40
rbtagroup <tigra.x@seznam.cz>
2025-05-30 23:48:42 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('rb-taxi-cache-v1').then(cache => {
+    caches.open('rb-taxi-cache-v2').then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
