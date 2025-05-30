38129bb5f41ff82924b0bb74bee795a4a2af8bf1
rbtagroup <tigra.x@seznam.cz>
2025-05-30 22:10:58 +0200

    Add files via upload
    oprava

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,18 @@
-
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('ridic-cache').then(cache => {
+    caches.open('ridic-app-cache-v1').then(cache => {
       return cache.addAll([
-        'index.html',
-        'manifest.json'
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/logo.png',
+        '/icons/icon-192x192.png',
+        '/icons/icon-256x256.png',
+        '/icons/icon-512x512.png'
       ]);
     })
   );
 });
-
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
