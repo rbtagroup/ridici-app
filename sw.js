ff10ef7caaf671129861f770a46d008d8a9ad942
rbtagroup <tigra.x@seznam.cz>
2025-05-31 22:12:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,46 @@
+
+const CACHE_NAME = 'rb-taxi-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/logo-header.png',
+  '/favicon.ico',
+  '/icon-192x192.png',
+  '/icon-256x256.png',
+  '/icon-512x512.png',
+  '/manifest.json'
+];
+
 self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
   self.skipWaiting();
 });
+
 self.addEventListener('activate', event => {
-  clients.claim();
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() =>
+        caches.match('/offline.html')
+      )
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => response || fetch(event.request))
+    );
+  }
 });
